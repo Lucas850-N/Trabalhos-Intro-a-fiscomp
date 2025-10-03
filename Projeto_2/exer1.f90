4dd651fe0a44d9637d6c61b4679d543b24ddca60
Lucas Nunes <lucasnunesalmeidaf@gmail.com>
2025-10-02 23:17:42 -0300

    Códigos prontos - 1° versão

--- Projeto_2/exer1.f90
+++ Projeto_2/exer1.f90
@@ -24,19 +24,19 @@ program exer1
 
 
         call calculo_preciso(x_0, ln, derivada_ln, derivada_2_ln, derivada_3_ln)
-        call derivada_1_2_pontos_frente(x_0, f_linha_1, derivada_ln, desvio_derivada_1, h, i, desvio_derivada_array_1)
-        call derivada_1_2_pontos_tras(x_0, f_linha_2, derivada_ln, desvio_derivada_2, h, i, desvio_derivada_array_2)
-        call derivada_1_3_pontos_simetricos(x_0, f_linha_3, derivada_ln, desvio_derivada_3, h, i, desvio_derivada_array_3)
-        call derivada_1_5_pontos_simetricos(x_0, f_linha_4, derivada_ln, desvio_derivada_4, h, i, desvio_derivada_array_4)
-        call derivada_2_3_pontos_simetricos(x_0, f_linha_5, derivada_2_ln, desvio_derivada_5, h, i, desvio_derivada_array_5)
-        call derivada_2_5_pontos_simetricos(x_0, f_linha_6, derivada_2_ln, desvio_derivada_6, h, i, desvio_derivada_array_6)
-        call derivada_3_5_pontos_antisimetricos(x_0, f_linha_7, derivada_3_ln, desvio_derivada_7, h, i, desvio_derivada_array_7)
+        call derivada_1_2_pontos_frente(x_0, f_linha_1, derivada_ln, desvio_derivada_1, h, dim_h, desvio_derivada_array_1)
+        call derivada_1_2_pontos_tras(x_0, f_linha_2, derivada_ln, desvio_derivada_2, h, dim_h, desvio_derivada_array_2)
+        call derivada_1_3_pontos_simetricos(x_0, f_linha_3, derivada_ln, desvio_derivada_3, h, dim_h, desvio_derivada_array_3)
+        call derivada_1_5_pontos_simetricos(x_0, f_linha_4, derivada_ln, desvio_derivada_4, h, dim_h, desvio_derivada_array_4)
+        call derivada_2_3_pontos_simetricos(x_0, f_linha_5, derivada_2_ln, desvio_derivada_5, h, dim_h, desvio_derivada_array_5)
+        call derivada_2_5_pontos_simetricos(x_0, f_linha_6, derivada_2_ln, desvio_derivada_6, h, dim_h, desvio_derivada_array_6)
+        call derivada_3_5_pontos_antisimetricos(x_0, f_linha_7, derivada_3_ln, desvio_derivada_7, h, dim_h, desvio_derivada_array_7)
 
         !Abre o arquivo tab1_out.dat e coloca os valores lá
         open(unit=2, file="tab1_out.dat", action="write")
-        write(2, "(A1, A50, A50, A50, A50, A50, A50)") "h", "derivada simétrica 3 pontos", "derivada para frente 2 pontos",&
-          & "derivada para trás 2 pontos", "derivada segunda simétrica 3 pontos", "derivada segunda simétrica 5 pontos",&
-          & "derivada terceira anti-simétrica 5 pontos"
+        write(2, "(5X,A1, 5X, A50, 5X, A29, 5X, A29, 5X, A36, 5X, A36, 5X, A42)") "h", "derivada simétrica 3 pontos", &
+          &"derivada para frente 2 pontos", "derivada para trás 2 pontos", "derivada segunda simétrica 3 pontos", &
+          &"derivada segunda simétrica 5 pontos", "derivada terceira anti-simétrica 5 pontos"
         do i = 1, dim_h
           write(2, *) h(i), desvio_derivada_array_3(i), desvio_derivada_array_1(i), desvio_derivada_array_2(i),&
             & desvio_derivada_array_5(i), desvio_derivada_array_6(i), desvio_derivada_array_7(i)
@@ -47,34 +47,27 @@ program exer1
         print *, ""
         print *, "Derivada simétrica 3 pontos:" 
         print *, "h = 0.000001"
-        print *, "Esse valor de h apresentou um desvio de 2.3004487204048019E-011, o menor quando comparado com o restante,&
-          & indicando maior precisão."
+        print *, "Justificativa: Esse valor de h apresentou maior precisão por ter o menor desvio, no valor de", desvio_derivada_array_3(12) 
         print *, ""
         print *, "Derivada para frente 2 pontos:"
         print *, "h = 0.00000001"
-        print *, "Esse valor de h apresentou um desvio de 7.9055879620426595E-009, o menor quando comparado com o restante,&
-          & indicando maior precisão."
+        print *, "Justificativa: Esse valor de h apresentou maior precisão por ter o menor desvio, no valor de", desvio_derivada_array_1(14)
         print *, ""
         print *, "Derivada para trás 2 pontos:"
         print *, "h = 0.00000001" 
-        print *, "Esse valor de h apresentou um desvio de 4.2108472264601460E-010, o menor quando comparado com o restante,&
-          & indicando maior precisão."
+        print *, "Justificativa: Esse valor de h apresentou maior precisão por ter o menor desvio, no valor de", desvio_derivada_array_2(14)
         print *, ""
         print *, "Derivada segunda simétrica 3 pontos"
         print *, "h = 0.0001"
-        print *, "Esse valor de h apresentou um desvio de 9.3870857753586279E-009, o menor quando comparado com o restante,&
-          & indicando maior precisão."
+        print *, "Justificativa: Esse valor de h apresentou maior precisão por ter o menor desvio, no valor de", desvio_derivada_array_5(9)
         print *, ""
         print *, "Derivada segunda simétrica 5 pontos"
         print *, "h = 0.001"
-        print *, "Esse valor de h apresentou um desvio de 1.2802425786162530E-011, o menor quando comparado com o restante,&
-          & indicando maior precisão."
+        print *, "Justificativa: Esse valor de h apresentou maior precisão por ter o menor desvio, no valor de", desvio_derivada_array_6(7)
         print *, ""
         print *, "Derivada terceira anti-simétrica 5 pontos"
         print *, "h = 0.0005"
-        print *, "Esse valor de h apresentou um desvio de 2.1206759273084685E-006, o menor quando comparado com o restante,&
-          & indicando maior precisão."
-
+        print *, "Justificativa: Esse valor de h apresentou maior precisão por ter o menor desvio, no valor de", desvio_derivada_array_7(8)
 
 
 
@@ -95,17 +88,18 @@ program exer1
                 end subroutine
 
                 !Subrotina para o cálculo da primeira derivada usando 2 pontos para frente
-                subroutine derivada_1_2_pontos_frente(x, f_linha, derivada_ln, desvio_derivada, h, i, desvio_derivada_array)
+                subroutine derivada_1_2_pontos_frente(x, f_linha, derivada_ln, desvio_derivada, h, dim_h, desvio_derivada_array)
 
                   !Declaração de variáveis
                   real(8), intent(in) :: x
                   real(8), intent(in) :: derivada_ln 
                   real(8), intent(out) :: f_linha, desvio_derivada
                   real(8), dimension(:), intent(in) :: h
-                  integer, intent(inout) :: i
+                  integer :: i
+                  integer, intent(in) :: dim_h
                   real(8),  dimension(:), intent(out) :: desvio_derivada_array
 
-                  do i = 1, size(h)
+                  do i = 1, dim_h
                     f_linha = (log(1.0d0+(x+h(i))**2) - log(1.0d0+x**2))/h(i)
                     desvio_derivada = abs(derivada_ln - f_linha)
                     desvio_derivada_array(i) = desvio_derivada
@@ -113,16 +107,17 @@ program exer1
                 end subroutine
 
                 !subrotina para o cálculo da primeira derivada usando 2 pontos para trás 
-                subroutine derivada_1_2_pontos_tras(x, f_linha, derivada_ln, desvio_derivada, h, i, desvio_derivada_array)
+                subroutine derivada_1_2_pontos_tras(x, f_linha, derivada_ln, desvio_derivada, h, dim_h, desvio_derivada_array)
 
                   real(8), intent(in) :: x
                   real(8), intent(in) :: derivada_ln
                   real(8), intent(out) :: f_linha, desvio_derivada
                   real(8), dimension(:), intent(in) :: h
-                  integer, intent(inout) :: i
+                  integer :: i
+                  integer, intent(in) :: dim_h
                   real(8), dimension(:), intent(out) :: desvio_derivada_array
 
-                    do i = 1, size(h)
+                    do i = 1, dim_h
                     f_linha = (log(1.0d0 + x**2) - log(1.0d0 + (x-h(i))**2)) / h(i)
                     desvio_derivada = abs(derivada_ln - f_linha)
                     desvio_derivada_array(i) = desvio_derivada
@@ -131,16 +126,17 @@ program exer1
                 end subroutine
 
                 !Subrotina para o cálculo da primeira derivada usando 3 pontos simétricos
-                subroutine derivada_1_3_pontos_simetricos(x, f_linha, derivada_ln, desvio_derivada, h, i, desvio_derivada_array)
+                subroutine derivada_1_3_pontos_simetricos(x, f_linha, derivada_ln, desvio_derivada, h, dim_h, desvio_derivada_array)
 
                   real(8), intent(in) :: x
                   real(8), intent(out) :: f_linha, desvio_derivada
                   real(8), intent(in) :: derivada_ln
                   real(8), dimension(:), intent(in) :: h
-                  integer, intent(inout) :: i
+                  integer :: i
+                  integer, intent(in) :: dim_h
                   real(8), dimension(:), intent(out) :: desvio_derivada_array
 
-                  do i = 1, size(h)
+                  do i = 1, dim_h
                     f_linha = (log(1.0d0+(x+h(i))**2) - log(1.0d0+(x-h(i))**2))/(2.0d0*h(i))
                     desvio_derivada = abs(derivada_ln - f_linha)
                     desvio_derivada_array(i) = desvio_derivada
@@ -149,16 +145,18 @@ program exer1
                 end subroutine
 
                 !Subrotina para o cálculo da primeira derivada usando 5 pontos simétricos 
-                subroutine derivada_1_5_pontos_simetricos(x, f_linha, derivada_ln, desvio_derivada, h, i, desvio_derivada_array)
+                subroutine derivada_1_5_pontos_simetricos(x, f_linha, derivada_ln, desvio_derivada, h, dim_h, &
+                    &desvio_derivada_array)
 
                   real(8), intent(in) :: x
                   real(8), intent(out) :: f_linha, desvio_derivada
                   real(8), intent(in) :: derivada_ln
                   real(8), dimension(:), intent(in) :: h
-                  integer, intent(inout) :: i
+                  integer :: i
+                  integer, intent(in) :: dim_h
                   real(8), dimension(:), intent(out) :: desvio_derivada_array
 
-                  do i = 1, size(h)
+                  do i = 1, dim_h
                     f_linha = ((log(1.0d0+(x-2.0d0*h(i))**2)) + (8.0d0*log(1.0d0+(x+h(i))**2)) - &
                     &(8.0d0*log(1.0d0+(x-h(i))**2)) - (log(1.0d0+(x+2.0d0*h(i))**2)))/(12.0d0*h(i)) !Talvez de erro
                     desvio_derivada = abs(derivada_ln - f_linha)
@@ -168,13 +166,15 @@ program exer1
                 end subroutine
 
                 !Subrotina para o cálculo da segunda derivada usando 3 pontos simétricos
-                subroutine derivada_2_3_pontos_simetricos(x, f_linha, derivada_2_ln, desvio_derivada, h, i, desvio_derivada_array)
+                subroutine derivada_2_3_pontos_simetricos(x, f_linha, derivada_2_ln, desvio_derivada, h, dim_h, &
+                    &desvio_derivada_array)
 
                   real(8), intent(in) :: x
                   real(8), intent(out) :: f_linha, desvio_derivada
                   real(8), intent(in) :: derivada_2_ln
                   real(8), dimension(:), intent(in) :: h
-                  integer, intent(inout) :: i
+                  integer :: i
+                  integer, intent(in) :: dim_h
                   real(8), dimension(:), intent(out) :: desvio_derivada_array
 
                   do i = 1, size(h)
@@ -186,16 +186,18 @@ program exer1
                 end subroutine
 
                 !Subrotina para o cálculo da segunda derivada usando 5 pontos simétricos
-                subroutine derivada_2_5_pontos_simetricos(x, f_linha, derivada_2_ln, desvio_derivada, h, i, desvio_derivada_array)
+                subroutine derivada_2_5_pontos_simetricos(x, f_linha, derivada_2_ln, desvio_derivada, h, dim_h, &
+                    &desvio_derivada_array)
 
                   real(8), intent(in) :: x
                   real(8), intent(out) :: f_linha, desvio_derivada
                   real(8), intent(in) :: derivada_2_ln
                   real(8), dimension(:) :: h
-                  integer, intent(inout) :: i
+                  integer :: i
+                  integer, intent(in) :: dim_h
                   real(8), dimension(:), intent(out) :: desvio_derivada_array
 
-                  do i = 1, size(h)
+                  do i = 1, dim_h
                     f_linha = ((-log(1.0d0+(x-2.0d0*h(i))**2)) + (16.0d0*log(1.0d0+(x+h(i))**2)) + (16.0d0*log(1.0d0+(x-h(i))**2)) - &
                     &(log(1.0d0+(x+2.0d0*h(i))**2)) - (30.0d0*(log(1.0d0+x**2))))/(12.0d0*(h(i))**2) !Talvez de erro
                     desvio_derivada = abs(derivada_2_ln - f_linha)
@@ -205,13 +207,15 @@ program exer1
                 end subroutine
 
                 !Subrotina para o cálculo da terceira derivada usando 5 pontos anti-simetricos
-                subroutine derivada_3_5_pontos_antisimetricos(x, f_linha, derivada_3_ln, desvio_derivada, h, i, desvio_derivada_array)
+                subroutine derivada_3_5_pontos_antisimetricos(x, f_linha, derivada_3_ln, desvio_derivada, h, dim_h, &
+                    &desvio_derivada_array)
 
                   real(8), intent(in) :: x
                   real(8), intent(out) :: f_linha, desvio_derivada
                   real(8), intent(in) :: derivada_3_ln
                   real(8), dimension(:), intent(in) :: h
-                  integer, intent(inout) :: i
+                  integer :: i
+                  integer, intent(in) :: dim_h
                   real(8), dimension(:), intent(out) :: desvio_derivada_array
 
                   do i = 1, size(h)
--- Projeto_2/exer2.f90
+++ Projeto_2/exer2.f90
@@ -1,9 +1,12 @@
 program exer2
 
   !Declaração de variáveis
-  implicit none 
-  integer :: dim_N
-  real(8), allocatable, dimension(:) :: N
+  implicit none
+  integer, parameter :: a = 0, b = 1
+  integer :: dim_N, i, j
+  real(8), allocatable, dimension(:) :: h, desvio_integral_simpson, desvio_integral_Bode, desvio_integral_trapezio
+  integer, allocatable, dimension(:) :: N
+  real(8) :: integral_precisa 
 
   !Abertura do arquivo tab2_in.dat para substituição de valores
   open(unit=1, file="tab2_in.dat", status="Old", action="Read")
@@ -12,13 +15,172 @@ program exer2
   read(1, *) N
   close(1)
 
+  allocate(h(dim_N), desvio_integral_trapezio(dim_N), desvio_integral_simpson(dim_N), desvio_integral_Bode(dim_N))
+
+  do i = 1, dim_N
+    h(i) = Real((b - a), 8)/Real(N(i), 8)
+  end do
+
+  call calculo_preciso_integral(integral_precisa)
+
+  call regra_do_trapezio(integral_precisa, desvio_integral_trapezio, h, N, dim_N)
+
+  call regra_de_Simpson(integral_precisa, desvio_integral_simpson, h, N, dim_N)
+
+  call regra_de_Bode(integral_precisa, desvio_integral_Bode, h, N, dim_N)
+
+  open(unit=2, file="tab2_out.dat", status="Replace", action="write")
+  write(2, "(10X, A1, 10X, A1, 22X, A18, 10X, A16, 9X, A13)") "N", "h", "Regra do trapézio", "Regra de Simpson", "Regra de Bode"
+  do i = 1, dim_N
+   write(2, *) N(i), h(i), desvio_integral_trapezio(i), desvio_integral_simpson(i), desvio_integral_Bode(i) 
+  end do
+  close(2)
+
+   print *, "Melhores valores de N:"
+   print *, ""
+   print *, "Regra do trapézio" 
+   print *, "N = 4096"
+   print *, "Justificativa: Esse valor de N apresentou maior precisão por ter o menor desvio, no valor de", desvio_integral_trapezio(11) 
+   print *, ""
+   print *, "Regra de Simpson"
+   print *, "N = 4096"
+   print *, "Justificativa: Esse valor de N apresentou maior precisão por ter o menor desvio, no valor de", desvio_integral_simpson(11)
+   print *, ""
+   print *, "Regra de Bode"
+   print *, "N = 2048" 
+   print *, "Justificativa: Esse valor de N apresentou maior precisão por ter o menor desvio, no valor de", desvio_integral_Bode(10)
 
   contains
-    subroutine calculo_preciso_integral
-      real(8) :: x, integral_precisa
+     
+    !Subrotina para o cálculo bruto da integral 
+    subroutine calculo_preciso_integral(integral_precisa)
+      real(8) :: integral_precisa
 
-      integral_precisa = 2*cos(1) - sen(1)
+      integral_precisa = 2.0d0*cos(1.0d0) - sin(1.0d0)
 
     end subroutine
 
+    !Subrotina para a regra do Trapézio
+    subroutine regra_do_trapezio(integral_precisa, desvio_integral, h, N, dim_N)
+      
+      !Declaração de variáveis
+      real(8) :: integral_trapezio, meio, meio_soma, extremidade_1, extremidade_2, x_j
+      real(8), intent(in) :: integral_precisa
+      real(8), dimension(:), intent(out) :: desvio_integral
+      real(8), dimension(:), intent(in) :: h
+      integer, dimension(:), intent(in) :: N
+      integer :: i, j
+      integer, intent(in) :: dim_N
+
+       do i = 1, dim_N
+       meio_soma = 0.0d0
+       integral_trapezio = 0.0d0
+
+        do j = 0, N(i)
+        x_j = j*h(i) 
+        
+        !Aqui, a expressão foi aberta e foi achado um padrão em que os extremos são multiplicados por 1 e todos os termos no meio
+        !por 2
+        if (j == 0) then
+          extremidade_1 = (h(i)/2.0d0)*((x_j**2)*cos(x_j))
+        else if (j == N(i)) then
+          extremidade_2 = (h(i)/2.0d0)*((x_j**2)*cos(x_j))
+        else
+          meio = h(i)*((x_j**2)*cos(x_j))
+          meio_soma = meio + meio_soma !Guarda e soma os valores do meio da expressão
+        end if
+          end do
+        integral_trapezio = extremidade_1 + extremidade_2 + meio_soma
+        desvio_integral(i) = abs(integral_trapezio - integral_precisa)
+      end do  
+    end subroutine
+
+    subroutine regra_de_Simpson(integral_precisa, desvio_integral, h, N, dim_N)
+
+      real(8) :: integral_Simpson, meio_soma_impar, meio_soma_par, extremidade_1, extremidade_2, meio_impar, meio_par, &
+        &meio_soma_pares, x_j
+      real(8), intent(in) :: integral_precisa
+      real(8), dimension(:), intent(out) :: desvio_integral
+      real(8), dimension(:), intent(in) :: h
+      integer, dimension(:), intent(in) :: N
+      integer :: i, j
+      integer, intent(in) :: dim_N
+
+
+       do i = 1, dim_N
+
+        integral_Simpson = 0.0d0
+        meio_soma_impar = 0.0d0
+        meio_soma_par = 0.0d0
+
+        do j = 0, N(i)
+        x_j = j*h(i) 
+        
+        if (j == 0) then
+          extremidade_1 = (h(i)/3.0d0)*((x_j**2)*cos(x_j))
+        else if (j == N(i)) then
+          extremidade_2 = (h(i)/3.0d0)*((x_j**2)*cos(x_j))
+        else if (mod(j,2) == 0) then !A diferença nessa regra para a última é que agora o coeficiente dos termos do meio são diferentes
+        !índices pares e ímpares (nossa condição confere se o índice é par)
+          meio_par = (2*h(i)/3)*((x_j**2)*cos(x_j))
+          meio_soma_par = meio_par + meio_soma_par
+        else if (mod(j,2) /= 0) then
+          meio_impar = (4*h(i)/3)*((x_j**2)*cos(x_j))
+          meio_soma_impar = meio_impar + meio_soma_impar
+        end if
+          end do
+        integral_Simpson = extremidade_1 + extremidade_2 + meio_soma_par + meio_soma_impar
+        desvio_integral(i) = abs(integral_Simpson - integral_precisa)
+      end do  
+    end subroutine 
+
+    !Subrotina para a regra de Bode
+    subroutine regra_de_Bode(integral_precisa, desvio_integral, h, N, dim_N)
+
+      real(8) :: integral_Bode, meio_impar, meio_par_sem4, meio_par_com4, meio_soma_impar, meio_soma_pares_com4, &
+      &extremidade_1, extremidade_2, meio_soma, meio_soma_pares_sem4, x_j
+      real(8), intent(in) :: integral_precisa
+      real(8), dimension(:), intent(out) :: desvio_integral
+      real(8), dimension(:), intent(in) :: h
+      integer, dimension(:), intent(in) :: N
+      integer :: i, j
+      integer, intent(in) :: dim_N
+
+       do i = 1, dim_N
+        integral_Bode = 0.0d0
+        meio_soma = 0.0d0
+        meio_soma_impar = 0.0d0
+        meio_soma_pares_com4 = 0.0d0
+        meio_soma_pares_sem4 = 0.0d0
+
+        do j = 0, N(i)
+        x_j = j*h(i) 
+        
+        if (j == 0) then
+          extremidade_1 = (7.0d0*2.0d0*h(i)/45.0d0)*((x_j**2)*cos(x_j))
+
+        else if (j == N(i)) then
+          extremidade_2 = (7.0d0*2.0d0*h(i)/45.0d0)*((x_j**2)*cos(x_j)) !Os termos nas extremidades serão multiplicados por 7
+
+        else if (mod(j,2) /= 0) then
+          meio_impar = (32.0d0*2.0d0*h(i)/45.0d0)*((x_j**2)*cos(x_j))
+          meio_soma_impar = meio_impar + meio_soma_impar
+
+        else if (mod(j,4) == 0) then 
+          meio_par_com4 = (14.0d0*2.0*h(i)/45.0d0)*((x_j**2)*cos(x_j))
+          meio_soma_pares_com4 = meio_par_com4 + meio_soma_pares_com4
+        
+        else if (mod(j,2) == 0) then !Aqui verificamos se o número é par e se é múltiplo de 4 para obedecer o padrão achado na
+        !expressão
+          meio_par_sem4 = (12.0d0*2.0d0*h(i)/45.0d0)*((x_j**2)*cos(x_j))
+          meio_soma_pares_sem4 = meio_par_sem4 + meio_soma_pares_sem4
+
+        end if
+          end do
+
+        integral_Bode = extremidade_1 + extremidade_2 + meio_soma_pares_sem4 + meio_soma_pares_com4 + meio_soma_impar
+        desvio_integral(i) = abs(integral_Bode - integral_precisa)
+      end do  
+  
+    end subroutine 
 end program exer2
